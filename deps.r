a384a0e1585a7a1cfc4964483ad84fdc4096749d
Ryan Jacobs <ryan.mjacobs@gmail.com>
2015-01-16 00:12:08 -0800

    Delete deps.r

--- deps.r
+++
@@ -1,2 +0,0 @@
-install.packages("plotGoogleMaps")
-packrat::snapshot()
---
+++ gen_map.r
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+################################################################################
+# gen_map.sh
+#
+# Generate a map with the geolocation data plotted.
+#
+# January 15, 2015
+################################################################################
+
+library("plotGoogleMaps")
